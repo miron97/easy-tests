26953abf0f2db1a8453a2df050678af3ff0d5d64
malinink <apmath@malinink.ru>
2016-11-09 21:44:27 +0300

    [cs] on users controller

--- src/main/java/easytests/controllers/UsersController.java
+++ src/main/java/easytests/controllers/UsersController.java
@@ -4,27 +4,26 @@ import easytests.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.ArrayList;
 
 
 /**
  * Created by fortyways on 03.11.16.
+ * Edited by malinink on 07.11.16
  */
 @Controller
 public class UsersController {
 
-    @RequestMapping(value="/users")
-    public String getUsers(Model model){
-        //Creating list
-        ArrayList<User> Users=new ArrayList<>();
-        //Filling the list
-        Users.add(new User("Dmitry","Alexeevich","Marulin"));
-        Users.add(new User("Zorigto","Zhargalovich","Dorzhiev"));
-        Users.add(new User("Djamil","Amirovich","Shaimordanov"));
-        //Transfer to view
-        model.addAttribute("Users",Users);
-        return "users";
-    }
+    @RequestMapping("/users")
+    public String list(Model model) {
 
+        ArrayList<User> users = new ArrayList<>(3);
+
+        users.add(new User("Dmitry","Alexeevich","Marulin"));
+        users.add(new User("Zorigto","Zhargalovich","Dorzhiev"));
+        users.add(new User("Djamil","Amirovich","Shaimordanov"));
+
+        model.addAttribute("users", users);
+        return "users/list";
+    }
 }
